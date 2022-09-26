9450f6f48a98cad25e81216502e51bbbc88e2567
Juan Giraldo <jusegiraldo76@gmail.com>
2022-09-26 06:57:36 -0500

    fixes

--- Game Assets/Pokemon style game.tsx
+++ Game Assets/Pokemon style game.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Pokemon style game" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
+<tileset version="1.5" tiledversion="1.7.2" name="sceneSpriteSheet" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
  <image source="tileset.png" width="384" height="384"/>
 </tileset>
